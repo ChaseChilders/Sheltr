9758adbb415903df8385b37ef70edf41b9d08cee
Chase Childers <chasechilders12@gmail.com>
2022-05-11 14:13:46 -0400

    added center map functions

--- script.js
+++ script.js
@@ -1,17 +1,38 @@
-// Initialize and add the map
-function initMap() {
-  // The location of Uluru
-  const uluru = { lat: -25.344, lng: 131.031 };
-  // The map, centered at Uluru
-  const map = new google.maps.Map(document.getElementById("map"), {
-    zoom: 4,
-    center: uluru,
-  });
-  // The marker, positioned at Uluru
+const cities = document.querySelector("#cities");
+
+cities.addEventListener("change", () => {
+  console.log("yay");
+  const dc = { lat: 38.9072, lng: -77.0369 };
   const marker = new google.maps.Marker({
-    position: uluru,
+    position: dc,
     map: map,
   });
+});
+
+function initMap() {
+  // The map, centered at DC
+  const map = new google.maps.Map(document.getElementById("map"), {
+    zoom: 4,
+    center: { lat: 48.394103, lng: -125.297251 },
+  });
+  window.map = map;
+  centerMap([
+    { lat: 48.394103, lng: -125.297251 },
+    { lat: 25.202134, lng: -66.086843 },
+  ]);
 }
 
 window.initMap = initMap;
+
+function centerMap(locations) {
+  const bounds = new google.maps.LatLngBounds();
+
+  for (i = 0; i < locations.length; i++) {
+    //extend the bounds to include each marker's position
+    // bounds.extend(new google.maps.LatLng(location.lat, location.lng));
+    bounds.extend(locations[i]);
+  }
+
+  //now fit the map to the newly inclusive bounds
+  map.fitBounds(bounds);
+}
